e686544cf7f2047caa58693e12ac8cc11b3775c1
Daniel Popescu <same.dan@gmail.com>
2022-05-17 14:20:03 +0200

    Updated wrong JWT secret

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -65,6 +65,7 @@ module.exports = {
   // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
+    console.log({ user });
 
     if (!user) {
       return ctx.badRequest(null, [
